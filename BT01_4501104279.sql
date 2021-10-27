545b8c76eae51067a1cfa10bf0b0954753356e66
Vinh Nguyen <4501104279@student.hcmue.edu.vn>
2021-10-27 13:05:24 +0700

    update file

--- BT01_4501104279.sql
+++ BT01_4501104279.sql
@@ -15,7 +15,18 @@ where ct.MACT = tg.MACT and
 		tg.SOTRAI >= 2
 --Cau 3
 go
-select MATRAN, NGAYTD, TENSAN, MACLB1, MACLB2,KETQUA
-from TRANDAU td, BANGXH bxh, SANVD svd, CAULACBO clb
-where td.MASAN = svd.MASAN and
-		
+select MATRAN, NGAYTD, TENSAN, clb1.TENCLB, clb2.TENCLB,KETQUA
+from TRANDAU td, SANVD svd, CAULACBO clb1, CAULACBO clb2
+where td.MASAN = svd.MASAN and 
+	MACLB1 = clb1.MACLB and
+	MACLB2 = clb2.MACLB and
+	td.VONG = 3 and td.NAM = 2009
+--Cau 4
+go
+select hlv.MAHLV, TENHLV, NGAYSINH, DIACHI, VAITRO, clb.TENCLB
+from HUANLUANVIEN hlv, HLV_CLB hlv_clb, CAULACBO clb, QUOCGIA qg
+where hlv.MAHLV = hlv_clb.MAHLV and
+		hlv_clb.MACLB = clb.MACLB
+		and hlv.MAQG = qg.MAQG
+		and qg.TENQG like N'%Việt Nam%'
+--Cau 5
